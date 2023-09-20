c0829f3aeb054e987549fac92df25452e2ff15e9
Amin Alian <alianamin00@gmail.com>
2023-09-20 15:56:08 +0200

    Added admin sign in button

--- EasyKeep/src/screens/HomePage.jsx
+++ EasyKeep/src/screens/HomePage.jsx
@@ -1,10 +1,12 @@
-import { HeaderBlue } from "../components/HeaderBlue";
 import { HeaderBrown } from "../components/HeaderBrown";
 
 export const HomePage = () => {
     return(
-        <div className="h-screen bg-eklightbrown">
+        <div className="flex flex-col h-screen bg-eklightbrown">
             <HeaderBrown />
+            <div className="flex h-3/5 items-center bg-black">
+                <button className="btn btn-lg">Sign in as admin</button>
+            </div>
         </div>
     );
 }
