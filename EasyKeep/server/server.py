214c2f39bef033d28b35cbca169a1db74bd328ce
Amin Alian <alianamin00@gmail.com>
2023-10-04 10:45:09 +0200

    Added taginfo endpoint

--- EasyKeep/server/server.py
+++ EasyKeep/server/server.py
@@ -1,6 +1,10 @@
 import sqlite3
-from flask import g
+from flask import Flask, g
+from flask_cors import CORS
+from markupsafe import escape
 
+app = Flask(__name__)
+CORS(app)
 DATABASE = './database.db'
 
 def get_db():
@@ -14,3 +18,30 @@ def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+
+@app.route("/")
+def index():
+    return ""
+
+@app.get("/status/<string:org>/<int:tag_id>")
+def get_tag_status(org, tag_id):
+    return {
+        "org": escape(org),
+        "tag": escape(tag_id),
+        "status:": 1
+    }
+
+@app.get("/tag/<string:org>/<int:tag_id>")
+def get_tag_info(org, tag_id):
+    return {
+        "org": escape(org),
+        "tag": escape(tag_id),
+        "name": "Amin Alian",
+        "status": 1
+    }
+
+@app.post("/authorize/<string:org>/<int:tag_id>")
+def authorize_tag(org, tag_id):
+    return {
+        "status": 200
+    }
