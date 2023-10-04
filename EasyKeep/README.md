513b6274e526449f2fcfbbb57108874aafa4ced0
Amin Alian <alianamin00@gmail.com>
2023-10-04 11:04:43 +0200

    Removed unneccesary dir

--- EasyKeep/README.md
+++
@@ -1,3 +0,0 @@
-# KeepIt
-
-KeepIt is a modern approach to wardrobes. To simplify the process of getting your jacket whenever you lose your tag KeepIt will serve you.
--- EasyKeep/src/components/Divider.jsx
+++
@@ -1,9 +0,0 @@
-export const Divider = ({color}) => {
-
-    return(
-        color === "ekblue" ?
-        <div className="h-12 bg-ekblue w-0.5" />
-        :
-        <div className="h-12 bg-eklightbrown w-0.5" />
-    );
-}
